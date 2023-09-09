c67d9ce1cdea3fb971f111e52b032b2afbeb7019
Khanh Truong <khanhtroung@gmail.com>
2023-09-09 12:15:22 -0700

    SE-87 * Make the navbar stick to the top of the page for each section

--- frontend/src/layouts/Navbar.tsx
+++ frontend/src/layouts/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 import { useState } from "react";
-import {FaShoppingCart} from "react-icons/fa"
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -18,15 +18,14 @@ const Navbar = () => {
     },
     { name: "Gallery", path: "/gallery" },
     { name: "Contact Us", path: "/contact" },
-    
   ];
   return (
     <nav
       className={`min-h-24 flex ${
         isExpanded
-          ? "flex-col lg:flex-row items-start gap-2 pb-4 lg:pb-0"
+          ? "flex-col items-start gap-2 pb-4 lg:flex-row lg:pb-0"
           : "flex-row"
-      } justify-between lg:justify-evenly 2xl:justify-center lg:items-center 2xl:gap-32 bg-gradient-to-bl from-amber-600 to-amber-500 lg:px-0 pl-1 pr-8`}
+      } sticky top-0 z-50 justify-between bg-gradient-to-bl from-amber-600 to-amber-500 pl-1 pr-8 lg:items-center lg:justify-evenly lg:px-0 2xl:justify-center 2xl:gap-32`}
     >
       <NavLink to="/" className="shrink-0">
         <img src="tea-rex-sign.png" alt="" className="max-h-24" />
@@ -34,24 +33,27 @@ const Navbar = () => {
       <ul
         className={`${
           isExpanded ? "flex" : "hidden lg:flex"
-        } pl-4 lg:pl-0 flex-col lg:flex-row gap-2 lg:gap-9 xl:gap-12`}
+        } flex-col gap-2 pl-4 lg:flex-row lg:gap-9 lg:pl-0 xl:gap-12`}
       >
         {routes.map((route) => (
           <NavLink to={route.path} key={route.name}>
-            <li className="hover:text-amber-100 duration-300 font-bold text-3xl hover:scale-110 transition font-navbar">
+            <li className="font-navbar text-3xl font-bold transition duration-300 hover:scale-110 hover:text-amber-100">
               {route.name}
             </li>
           </NavLink>
         ))}
       </ul>
-      <NavLink to="/cart" className="text-3xl bg-lime-700 text-white font-bold rounded-full px-3 py-3 hover:scale-110 transition">
-          <FaShoppingCart />
+      <NavLink
+        to="/cart"
+        className="rounded-full bg-lime-700 px-3 py-3 text-3xl font-bold text-white transition hover:scale-110"
+      >
+        <FaShoppingCart />
       </NavLink>
       <NavLink
         to="/menu"
         className={`${
           isExpanded ? "block" : "hidden"
-        } text-2xl bg-lime-700 text-white font-bold rounded-full ml-2 px-4 py-2 lg:ml-0 lg:px-6 lg:py-3 hover:scale-110 transition lg:block`}
+        } ml-2 rounded-full bg-lime-700 px-4 py-2 text-2xl font-bold text-white transition hover:scale-110 lg:ml-0 lg:block lg:px-6 lg:py-3`}
       >
         ORDER NOW
       </NavLink>
