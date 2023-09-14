8bb106b0213695ba0b7860d0bcceb599d0ff91ae
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2023-09-14 14:34:26 -0700

    SE-24: added printer component for clover api

---
+++ frontend/src/components/Printer.tsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react"
+
+export default function Printer() {
+  const apiToken = import.meta.env.VITE_APP_CLOVER_PRINTER_API_TOKEN
+  const mid = 'CVMBG5FBAFSF1'//merchant ID
+
+  interface Order {
+    orderId: string,
+    // email: string,
+    items: [
+      {
+        amount: number,
+        description: string
+      }
+    ]
+  }
+
+  const [order, setOrder] = useState<Order>(
+    {
+      orderId: '',
+      // email: '',
+      items: [
+        {
+          amount: 1,
+          description: 'food'
+        }
+      ]
+    }
+  )
+
+  const submitPrintRequest = (oid: string) => { //oid = orderId
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        authorization: `Bearer ${apiToken}`
+      },
+      body: JSON.stringify({orderRef: {id: oid}/*, id: id*/})
+    }
+    fetch(`https://sandbox.dev.clover.com/v3/merchants/${mid}/print_event`, options)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+  }
+
+  // // order needs to be created before you can send a printRequest to the printer
+  const createOrder = (order: Order) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        authorization: `Bearer ${apiToken}`
+      },
+      body: JSON.stringify({
+        currency: 'USD',
+        // email: order.email,
+        items: order.items.map((item) => ({
+              amount: item.amount,
+              description: item.description,
+              // inventory_id: order.items.inventory_id
+          }))
+      })
+    }
+    fetch(`https://sandbox.dev.clover.com/v3/merchants/${mid}/orders`, options)
+    .then(res => res.json())
+    .then(res => 
+      {
+        // console.log(res)
+        submitPrintRequest(res.id)
+      })
+    .catch(err => console.error(err))
+  }
+
+  return (
+    // this button was used for testing
+    <>
+    <div>
+      <button onClick={() => createOrder(order)}>print order</button>
+    </div>
+    </>
+  )
+}
+
+// createOrder(order)
