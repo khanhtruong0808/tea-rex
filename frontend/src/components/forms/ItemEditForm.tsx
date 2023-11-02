355d13eff19663b0437f20d0c4f703911431ec49
Khanh Truong <khanhtroung@gmail.com>
2023-11-01 22:11:24 -0700

    add spinner component to the rest of the forms

--- frontend/src/components/forms/ItemEditForm.tsx
+++ frontend/src/components/forms/ItemEditForm.tsx
@@ -4,6 +4,7 @@ import { toast } from "react-hot-toast";
 import { useState } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import useDialog from "../../utils/dialogStore";
+import { Spinner } from "../../utils/Spinner";
 
 export const ItemEditForm = ({ item }: { item: MenuItem }) => {
   const [loading, setLoading] = useState(false);
@@ -109,30 +110,7 @@ export const ItemEditForm = ({ item }: { item: MenuItem }) => {
           loading && "cursor-not-allowed opacity-50"
         } mb-2 mr-2 w-24 self-start rounded-lg bg-lime-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-lime-800`}
       >
-        {loading ? (
-          <svg
-            className="mx-auto h-5 w-5 animate-spin text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-        ) : (
-          "Submit"
-        )}
+        {loading ? <Spinner /> : "Submit"}
       </button>
     </form>
   );
